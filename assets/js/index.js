b67987b647c271618b2bccc427d4f5b50fec05d6
Olga Senakosava <senakosava.olga@gmail.com>
2022-01-30 20:10:00 +0100

    added form and made it work
    ya know, the mail sending thing, it was a mess

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,5 @@
 
-
-
+      /*  Menu  */
 
   function openMenu() {
     document.getElementById("menu-normal").classList.toggle("show");
@@ -19,3 +18,23 @@
     }
   }
 }
+
+
+
+     /*  Form  */
+
+
+       window.onload = function() {
+           document.getElementById('contact-form').addEventListener('submit', function(event) {
+               event.preventDefault();
+               // generate a five digit number for the contact_number variable
+               this.contact_number.value = Math.random() * 100000 | 0;
+               // these IDs from the previous steps
+               emailjs.sendForm('gmailOlga', 'defaultTemplate', this)
+                   .then(function() {
+                       console.log('SUCCESS!');
+                   }, function(error) {
+                       console.log('FAILED...', error);
+                   });
+           });
+       }
